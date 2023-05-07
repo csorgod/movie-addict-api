462b09c1a5fb7f232b1c8a6acb7f339ad534a3b6
Guilherme Csorgo Henriques <csorgodev@hotmail.com>
2023-05-07 19:47:28 -0300

    Added test configs using pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 from typing import Any, Generator
 
-import pytest
+from pytest import fixture
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 
@@ -9,14 +9,14 @@ from db import db, engine, session_local
 from src.dependencies.database import get_db
 
 
-@pytest.fixture(autouse=True)
+@fixture(autouse=True)
 def app() -> Generator[FastAPI, Any, None]:
     db.metadata.create_all(bind = engine)
     _app = get_application()
     yield _app
     db.metadata.drop_all(bind = engine)
 
-@pytest.fixture
+@fixture
 def db_session() -> Generator[session_local, Any, None]:
     # Start the connection and yield until the end of the transaction
     connection = engine.connect()
@@ -29,7 +29,7 @@ def db_session() -> Generator[session_local, Any, None]:
     transaction.rollback()
     connection.close()
 
-@pytest.fixture()
+@fixture()
 def client(app: FastAPI, db_session: session_local) -> Generator[TestClient, Any, None]:
 
     # Yields a db_session using the fixture `db_session`
