66fd80d881af123375e1db044878d10d60084f04
Guilherme Csorgo Henriques <csorgodev@hotmail.com>
2023-05-13 18:18:14 -0300

    Changed mysql port in code

--- db.py
+++ db.py
@@ -9,7 +9,7 @@ mysql_db_name = os.getenv('MYSQL_DBNAME', 'movieaddictdb')
 mysql_db_username = os.getenv('MYSQL_USER', 'movieaddictrestapiuserdb')
 mysql_db_user_password = os.getenv('MYSQL_PASSWORD', 'movieaddicrestapiuserpassword')
 mysql_service_host = os.getenv('MYSQL_SERVICE_HOST', 'localhost')
-mysql_service_port = int(os.getenv('MYSQL_SERVICE_PORT', 33306))
+mysql_service_port = int(os.getenv('MYSQL_SERVICE_PORT', 3306))
 
 SQLALCHEMY_DATABASE_URL = f'mysql+pymysql://{mysql_db_username}:{mysql_db_user_password}@{mysql_service_host}:{mysql_service_port}/{mysql_db_name}?charset=utf8mb4'
 SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('DB_TRACK_MODIFICATIONS', False)
